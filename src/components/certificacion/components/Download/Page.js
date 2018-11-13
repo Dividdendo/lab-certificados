8a2e3be2e66fb10325664097a890f40d66684064
rails -v <nleguizamon@dividendoporcolombia.org>
2018-11-13 18:15:44 -0500

    Cositas finales

--- src/components/certificacion/components/Download/Page.js
+++ src/components/certificacion/components/Download/Page.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const Page = ({children, singleMode, id}) => (<div
   id={id}  
   style={{width: "210mm", height: singleMode ? "297mm" : ""}}
---
+++ src/components/shared/erros.js/danger.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+class Danger extends React.Component {
+    render() {
+        return("Error a sucesido algo peligroso ");
+    }
+}
+export default Danger;
